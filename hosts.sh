14c06d7159966e7ae26c709a65f8754e0615e68c
David <dafespinelsa@unal.edu.co>
2016-12-15 13:33:52 +0100

    test for the /etc/hosts file

--- hosts.sh
+++ hosts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo '192.168.0.1 page1.com' >> /etc/hosts
-echo '192.168.0.2 page2.com' >> /etc/hosts
-echo '192.168.0.3 page3.com' >> /etc/hosts
-echo '192.168.0.4 page4.com' >> /etc/hosts
+echo '192.168.0.4 page1.com' >> /etc/hosts
+echo '192.168.0.4 page2.com' >> /etc/hosts
+echo '192.168.0.4 page3.com' >> /etc/hosts
+echo '192.168.0.7 page4.com' >> /etc/hosts
 
